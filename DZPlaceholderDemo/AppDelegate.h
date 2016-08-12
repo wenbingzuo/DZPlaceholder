fbb441fa85e29bf540e7e202d43bae3e8bfd5898
DaZuo <Jaly201319@hotmail.com>
2016-08-12 10:49:37 +0800

    Initial Commit

---
+++ DZPlaceholderDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DZPlaceholderDemo
+//
+//  Created by Wenbing Zuo on 8/12/16.
+//  Copyright © 2016 Wenbing Zuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
